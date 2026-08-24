8da004a1232ba426d948b3fc1b3b90fb3c956e75
Fabien Hummel <18258767+fabienhummel@users.noreply.github.com>
2026-08-24 11:01:51 +0200

    feat: add simple tournament controller skeleton

---
+++ controllers/tournament_controller.py
@@ -0,0 +1,26 @@
+class TournamentController:
+    """Manage tournament-related actions."""
+
+    def create_tournament(self):
+        """Create a tournament."""
+        pass
+
+    def add_player(self):
+        """Add a player to a tournament."""
+        pass
+
+    def create_round(self):
+        """Create a tournament round."""
+        pass
+
+    def create_matches(self):
+        """Create matches for a round."""
+        pass
+
+    def record_result(self):
+        """Record a match result."""
+        pass
+
+    def close_round(self):
+        """Close a round."""
+        pass
