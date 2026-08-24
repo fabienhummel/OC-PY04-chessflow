eb8e5a76943f8df257a41f76c5fda2abdf24b368
Fabien Hummel <18258767+fabienhummel@users.noreply.github.com>
2026-08-24 11:17:09 +0200

    feat: implement simple player controller

--- controllers/player_controller.py
+++ controllers/player_controller.py
@@ -1,10 +1,19 @@
+from models.player import Player
+
+
 class PlayerController:
     """Manage player-related actions."""
 
-    def create_player(self):
+    def __init__(self):
+        """Initialize the player controller."""
+        self.players = []
+
+    def create_player(self, last_name, first_name, birth_date, national_id):
         """Create a player."""
-        pass
+        player = Player(last_name, first_name, birth_date, national_id)
+        self.players.append(player)
+        return player
 
     def list_players(self):
         """List all players."""
-        pass
+        return self.players
