2b52c48dea450383b630ca0fd596aeadb0cf98c3
Fabien Hummel <18258767+fabienhummel@users.noreply.github.com>
2026-08-24 10:36:50 +0200

    feat: implement simple player serialization

--- models/player.py
+++ models/player.py
@@ -10,9 +10,19 @@ class Player:
 
     def to_dict(self):
         """Convert the player to a dictionary."""
-        pass
+        return {
+            "last_name": self.last_name,
+            "first_name": self.first_name,
+            "birth_date": self.birth_date,
+            "national_id": self.national_id,
+        }
 
     @classmethod
     def from_dict(cls, data):
         """Create a player from a dictionary."""
-        pass
+        return cls(
+            data["last_name"],
+            data["first_name"],
+            data["birth_date"],
+            data["national_id"],
+        )
