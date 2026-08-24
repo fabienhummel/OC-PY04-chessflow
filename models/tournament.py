58ff16c6a264d332f6903fcbfaf787f56f0d4523
Fabien Hummel <18258767+fabienhummel@users.noreply.github.com>
2026-08-24 10:47:13 +0200

    feat: implement simple tournament methods

--- models/tournament.py
+++ models/tournament.py
@@ -1,3 +1,7 @@
+from models.player import Player
+from models.round import Round
+
+
 class Tournament:
     """Represent a chess tournament."""
 
@@ -23,17 +27,39 @@ class Tournament:
 
     def add_player(self, player):
         """Add a player to the tournament."""
-        pass
+        self.players.append(player)
 
     def add_round(self, round_):
         """Add a round to the tournament."""
-        pass
+        self.rounds.append(round_)
+        self.current_round += 1
 
     def to_dict(self):
         """Convert the tournament to a dictionary."""
-        pass
+        return {
+            "name": self.name,
+            "location": self.location,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "description": self.description,
+            "number_of_rounds": self.number_of_rounds,
+            "current_round": self.current_round,
+            "players": [player.to_dict() for player in self.players],
+            "rounds": [round_.to_dict() for round_ in self.rounds],
+        }
 
     @classmethod
     def from_dict(cls, data):
         """Create a tournament from a dictionary."""
-        pass
+        tournament = cls(
+            data["name"],
+            data["location"],
+            data["start_date"],
+            data["end_date"],
+            data["description"],
+            data["number_of_rounds"],
+        )
+        tournament.current_round = data["current_round"]
+        tournament.players = [Player.from_dict(player) for player in data["players"]]
+        tournament.rounds = [Round.from_dict(round_) for round_ in data["rounds"]]
+        return tournament
