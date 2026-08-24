3ef2059e44d96c2b6ea640421964d64a7b92a5c9
Fabien Hummel <18258767+fabienhummel@users.noreply.github.com>
2026-08-24 10:31:20 +0200

    docs: add simple docstrings to Match model

--- models/match.py
+++ models/match.py
@@ -1,16 +1,22 @@
 class Match:
+    """Represent a chess match between two players."""
+
     def __init__(self, player_one, player_two):
+        """Initialize a match."""
         self.player_one = player_one
         self.player_two = player_two
         self.score_one = None
         self.score_two = None
 
     def set_result(self, score_one, score_two):
+        """Set the result of the match."""
         pass
 
     def to_dict(self):
+        """Convert the match to a dictionary."""
         pass
 
     @classmethod
     def from_dict(cls, data):
+        """Create a match from a dictionary."""
         pass
