f5f9bebd101fe7f872673cbe6e9075b68731f1c2
Fabien Hummel <18258767+fabienhummel@users.noreply.github.com>
2026-08-24 11:13:34 +0200

    feat: initialize application controller

--- controllers/application_controller.py
+++ controllers/application_controller.py
@@ -1,5 +1,14 @@
+from controllers.player_controller import PlayerController
+from controllers.tournament_controller import TournamentController
+
+
 class ApplicationController:
     """Control the main application flow."""
+
+    def __init__(self):
+        """Initialize the application controller."""
+        self.player_controller = PlayerController()
+        self.tournament_controller = TournamentController()
 
     def run(self):
         """Run the application."""
