e1ec1e1205b75d93be44463c322712523b9b58f1
Fabien Hummel <18258767+fabienhummel@users.noreply.github.com>
2026-08-24 10:31:14 +0200

    docs: add simple docstrings to Player model

--- models/player.py
+++ models/player.py
@@ -1,13 +1,18 @@
 class Player:
+    """Represent a chess player."""
+
     def __init__(self, last_name, first_name, birth_date, national_id):
+        """Initialize a player."""
         self.last_name = last_name
         self.first_name = first_name
         self.birth_date = birth_date
         self.national_id = national_id
 
     def to_dict(self):
+        """Convert the player to a dictionary."""
         pass
 
     @classmethod
     def from_dict(cls, data):
+        """Create a player from a dictionary."""
         pass
