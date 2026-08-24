23fc3864269265504bafc22bb73e2f2e9fe863a8
Fabien Hummel <18258767+fabienhummel@users.noreply.github.com>
2026-08-24 11:01:46 +0200

    feat: add simple player controller skeleton

---
+++ controllers/player_controller.py
@@ -0,0 +1,10 @@
+class PlayerController:
+    """Manage player-related actions."""
+
+    def create_player(self):
+        """Create a player."""
+        pass
+
+    def list_players(self):
+        """List all players."""
+        pass
