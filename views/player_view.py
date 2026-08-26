b34e31498bccc9569fbe9d5c87dc0082c064f864
Fabien Hummel <18258767+fabienhummel@users.noreply.github.com>
2026-08-26 15:15:11 +0200

    feat: add player view prototype

---
+++ views/player_view.py
@@ -0,0 +1,10 @@
+class PlayerView:
+    """Display player-related information."""
+
+    def get_player_data(self):
+        """Get player data from the user."""
+        pass
+
+    def display_players(self, players):
+        """Display a list of players."""
+        pass
