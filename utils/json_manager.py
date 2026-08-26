a08e25a9ab2f92357980bd224d4f69883d6a0939
Fabien Hummel <18258767+fabienhummel@users.noreply.github.com>
2026-08-26 14:07:10 +0200

    feat: add JSON manager prototype

---
+++ utils/json_manager.py
@@ -0,0 +1,46 @@
+"""Manage JSON data for players and tournaments."""
+
+
+def create_player(player):
+    """Save a new player."""
+    pass
+
+
+def load_players():
+    """Load all players."""
+    pass
+
+
+def update_player(player):
+    """Update an existing player."""
+    pass
+
+
+def delete_player(national_id):
+    """Delete a player."""
+    pass
+
+
+def create_tournament(tournament):
+    """Save a new tournament."""
+    pass
+
+
+def load_tournaments():
+    """Load all tournaments."""
+    pass
+
+
+def load_tournament(name):
+    """Load one tournament."""
+    pass
+
+
+def update_tournament(tournament):
+    """Update an existing tournament."""
+    pass
+
+
+def delete_tournament(name):
+    """Delete a tournament."""
+    pass
