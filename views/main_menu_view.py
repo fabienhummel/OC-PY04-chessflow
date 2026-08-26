6d3612d891b65bcbf884272efa4786c27f41111a
Fabien Hummel <18258767+fabienhummel@users.noreply.github.com>
2026-08-26 15:15:04 +0200

    feat: add main menu view prototype

---
+++ views/main_menu_view.py
@@ -0,0 +1,10 @@
+class MainMenuView:
+    """Display the main menu."""
+
+    def display_menu(self):
+        """Display the main menu."""
+        pass
+
+    def get_choice(self):
+        """Get the user choice."""
+        pass
