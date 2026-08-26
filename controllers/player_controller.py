6430fec6db2b1cbc77d809227ccefbb4f149638e
Fabien Hummel <18258767+fabienhummel@users.noreply.github.com>
2026-08-26 14:19:57 +0200

    feat: connect player controller to JSON storage

--- controllers/player_controller.py
+++ controllers/player_controller.py
@@ -1,4 +1,5 @@
 from models.player import Player
+from utils.json_manager import load_players, save_players
 
 
 class PlayerController:
@@ -6,12 +7,13 @@ class PlayerController:
 
     def __init__(self):
         """Initialize the player controller."""
-        self.players = []
+        self.players = load_players()
 
     def create_player(self, last_name, first_name, birth_date, national_id):
         """Create a player."""
         player = Player(last_name, first_name, birth_date, national_id)
         self.players.append(player)
+        save_players(self.players)
         return player
 
     def list_players(self):
