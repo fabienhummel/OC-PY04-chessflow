a3b541127fcc67a210514f393d4d29ca11899063
Fabien Hummel <18258767+fabienhummel@users.noreply.github.com>
2026-08-26 15:15:27 +0200

    feat: add report view prototype

---
+++ views/report_view.py
@@ -0,0 +1,18 @@
+class ReportView:
+    """Display application reports."""
+
+    def display_players(self, players):
+        """Display players."""
+        pass
+
+    def display_tournaments(self, tournaments):
+        """Display tournaments."""
+        pass
+
+    def display_tournament_details(self, tournament):
+        """Display tournament details."""
+        pass
+
+    def display_rounds(self, rounds):
+        """Display tournament rounds and matches."""
+        pass
