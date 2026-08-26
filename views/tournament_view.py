c491851a7bc0e1aa71803cc4ed2ee29930a1839b
Fabien Hummel <18258767+fabienhummel@users.noreply.github.com>
2026-08-26 15:15:19 +0200

    feat: add tournament view prototype

---
+++ views/tournament_view.py
@@ -0,0 +1,14 @@
+class TournamentView:
+    """Display tournament-related information."""
+
+    def get_tournament_data(self):
+        """Get tournament data from the user."""
+        pass
+
+    def display_tournament(self, tournament):
+        """Display a tournament."""
+        pass
+
+    def get_match_result(self, match):
+        """Get a match result from the user."""
+        pass
